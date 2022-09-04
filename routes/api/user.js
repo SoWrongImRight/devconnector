ed14a4ec8ae5c4433bd6ce398a2379cc3514bbe4
Russ Carroll <info@rcarroll.dev>
2022-09-03 22:16:04 -0400

    Backend commpleted.

---
+++ routes/api/user.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const User = require('../../models/User')
+
+// @route GET api/users
+// @desc Register user
+// @access Public
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Please enter a password with six or more characters').isLength({ min: 6 })
+], 
+async (req,res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()})
+  }
+
+  const { name, email, password } = req.body;
+
+  try {
+    // See if user exists
+    let user = await User.findOne({ email });
+
+    if(user) {
+      return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+    }
+  
+    // Get user's gravatar
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    })
+  
+    // Encrypt password
+    user = new User({
+      name,
+      email,
+      avatar,
+      password
+    })
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+  
+    // Return JWT
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if(err) throw err;
+        return res.json({ token })
+      }
+    )
+
+  } catch(err) {
+    console.log(err.message)
+    return res.status(500).send('Server Error')
+  }
+  
+
+
+  // res.send('User registered!');
+});
+
+module.exports = router;
